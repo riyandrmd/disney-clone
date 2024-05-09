c5a58362ae2c21870f9cb9d5fb54966fe4663f59
riyandrmd <ryanda.rama10@gmail.com>
2024-05-09 14:33:42 +0700

    create header

--- src/Components/HeaderItem.jsx
+++ src/Components/HeaderItem.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function HeaderItem() {
-  return (
-    <div>HeaderItem</div>
-  )
+function HeaderItem({name,Icon}) {
+    return (
+        <div className='text-white flex items-center gap-3 text-[15px] font-semibold cursor-pointer underline-offset-8 hover:underline'>
+            <Icon />
+            <h2>{name}</h2>  
+        </div>
+    )
 }
 
 export default HeaderItem
